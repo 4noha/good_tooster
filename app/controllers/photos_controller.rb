6b244bf59b84f46a882ccbb262096c23ace73513
nokkii <nokkii.03@gmail.com>
2015-09-22 20:02:02 +0900

    Rmagickの処理を追加

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -15,7 +15,14 @@ class PhotosController < ApplicationController
       puts params[:time_name].split("\"")[1]
       toast = ToastTimeLapse.find_by(mm_ss: params[:time_name].split("\"")[1])
       puts toast
-      toast.image = Base64.encode64(params[:image].read)
+      
+      # Rmagickの画処理
+      image_magick = Magick::Image.from_blob(params[:image].read).shift
+      image_magick.strip!
+      image_magick = image_magick.resize_to_fill(400, 640)
+      image_magick.format = 'JPG'
+      toast.image = Base64.encode64(image_magick.to_blob {self.quality = 70})
+      
       toast.save
       return redirect_to controller: :tops
     end
