e396647f049066ad183b39135ffa46d55ddaa18d
nokkii <nokkii.03@gmail.com>
2017-12-18 23:41:09 +0900

    delete rails files

--- app/models/toast_time_lapse.rb
+++
@@ -1,2 +0,0 @@
-class ToastTimeLapse < ActiveRecord::Base
-end
--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  root to: "tops#index"
-    resource :photos
-    resources :apis do
-    collection do
-      get :op
-    end
-  end
-end
